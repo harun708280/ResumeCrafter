d9b5447413391491513bab5cfc8bf863ca583b91
harunbd80 <harun708280@gmail.com>
2025-01-26 21:40:12 +0600

    add resume file

---
+++ src/Dashboard/AddResume.jsx
@@ -0,0 +1,26 @@
+import { PlusSquare } from 'lucide-react';
+import React from 'react';
+
+const AddResume = () => {
+    return (
+        <div>
+            <div >
+        <div className='p-14 py-24 border 
+        items-center flex 
+        justify-center bg-secondary
+        rounded-lg h-[280px]
+        hover:scale-105 transition-all hover:shadow-md
+        cursor-pointer border-dashed'
+        onClick={()=>setOpenDialog(true)}
+        >
+            <PlusSquare  />
+        </div>
+
+        
+
+    </div>
+        </div>
+    );
+};
+
+export default AddResume;
--- src/Dashboard/Dashboard.jsx
+++ src/Dashboard/Dashboard.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React from "react";
+import AddResume from "./AddResume";
 
 const Dashboard = () => {
-    return (
-        <div>
-            dasboadrd
-        </div>
-    );
+  return (
+    <div className="p-10 md:px-20 lg:px-32">
+      <h2 className="font-bold text-3xl">My Resume</h2>
+      <p>Start Creating AI resume to your next Job role</p>
+      <div
+        className="grid grid-cols-2 
+      md:grid-cols-3 lg:grid-cols-5 gap-5
+      mt-10"
+      >
+
+        <AddResume></AddResume>
+
+       {[1,2,3,4].map((item,index)=>(
+          <div className='h-[280px] rounded-lg bg-slate-200 animate-pulse'>
+          </div>
+        )) }
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
